7c5c34c1de6a3cad193981eeebb9a1d86c1d585b
ErjonR <erjonjashari08@gmail.com>
2025-02-01 07:19:57 -0800

    .

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$num = 5;
+if($num < 0){
+    echo "$num is less tyhan 0";
+}else{
+    echo "$num is bigger than 0";
+}
+$age = 15;
+if(($age > 12) && ($age<20)){
+    echo "You are a teenager <br>";
+}
+if($num <0){
+    echo "The value of the number is negative"
+} elseIF {
+
+}
+?> 
+
