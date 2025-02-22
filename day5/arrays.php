2ed6a5c99b22fbac4ecaa15dfcde5b270f8869c0
ErjonR <erjonjashari08@gmail.com>
2025-02-22 07:38:12 -0800

    day5

---
+++ day5/arrays.php
@@ -0,0 +1,19 @@
+<?php
+
+$grade = array(
+    "Math" => "4",
+    "Art" => "5",
+    "History" => "3",
+    "Music" => "5",
+);
+
+
+foreach ($grade as $subject => $grade):{
+    echo "subject: ".$subject . "-> Grade: ".$grade;
+    echo "<br>";
+}
+
+
+
+
+?>
