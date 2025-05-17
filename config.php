c74f9cf8a0fdee4dd2ce54467bc721fa9a9cb526
ErjonR <erjonjashari08@gmail.com>
2025-05-17 07:42:13 -0700

    day10

--- config.php
+++ config.php
@@ -1,19 +1,19 @@
 <?php
+    session_start();
 
-session_start();
+    $server = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'dbtest1';
 
-$server = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'dbtest1';
+    try{
+        $connect = new PDO("mysql:host=$server;dbname=$dbname", $username, $password);
 
-try {
-    $connect = new PDO("mysql:host=$server;dbname=$dbname", $username, $password);
-    // Enable error mode to throw exceptions
-    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $e) {
-    // Display the error message
-    echo "Connection failed: " . $e->getMessage();
-    die();
-}
+    }catch(Exeption $e){
+        echo "Something went wrong";
+    }
+
+
+
+
 ?>
--- day10/loginlogic.php
+++ day10/loginlogic.php
@@ -1,34 +1,37 @@
 <?php
-require 'config.php';
+    require 'config.php';
 
-if(isset($_POST['submit']))
-{
-
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    if(empty($username) || empty($password))
+    if(isset($_POST['submit']))
     {
-        echo "Fill all the fields"!;
-        header("refresh:2 url=signin.php");
-    }else{
-        $sql = "SELECT * FROM users WHERE username = :username";
-        $insertSQL = $conn->prepare($sql);
-        $insertSQL->bindParam(':username', $username);
-        $insertSQL->execute();
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
-        if($insertSQl->rowCount()> 0){
-            $data = $insertSQL->fetch();
-            if(password_verify($password, $data['password'])){
-                $_SESSION['username']= $data['username']
-                header("Location:dashboard.php");
-            }else{
-                echo "Password incorrect";
-                header("refresh:2 url=signin.php");
-            }
-
+        if(empty($username) || empty($password))
+        {
+            echo "Fill all the fields!";
+            header("refresh:2 url=signin.php");
         }else{
-            echo "Users not found!"
+            $sql = "SELECT * FROM users WHERE username = :username";
+            $insertSQL = $connect->prepare($sql);
+            $insertSQL->bindParam(':username', $username);
+            $insertSQL->execute();
+
+            if($insertSQL->rowCount()> 0){
+                $data = $insertSQL->fetch();
+                if(password_verify($password, $data['password'])){
+                    $_SESSION['username']= $data['username'];
+                    header("Location:dashboard.php");
+                }else{
+                    echo "Password incorrect";
+                    header("refresh:2 url=signin.php");
+                }
+            }else{
+                echo "User not found!";
+            }
         }
     }
-}
+
+
+
+
+?>
