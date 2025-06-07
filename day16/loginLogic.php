d2e84d601f97a6a3e653ae97b359cc19f206f57c
ErjonR <erjonjashari08@gmail.com>
2025-06-07 07:41:32 -0700

    commit

--- day16/loginLogic.php
+++ day16/loginLogic.php
@@ -1,18 +1,36 @@
 <?php
-
 session_start();
-include_once('submit');
+include_once('config.php');
 
-
-if(isset($_POST['submit'])){
-    $username = $_POST['username'];
+if (isset($_POST['submit'])) {
+    $username = trim($_POST['username']);
     $password = $_POST['password'];
+
     if (empty($username) || empty($password)) {
-        echo "Please fill in all flieds";
+        echo "Please fill in all fields";
+    } else {
+        $sql = "SELECT id, username, email, password, is_admin FROM users WHERE username = :username";
+        $selectUser = $config->prepare($sql);
+        $selectUser->bindParam(":username", $username);
+        $selectUser->execute();  // Execute the query
+
+        $data = $selectUser->fetch(PDO::FETCH_ASSOC);  // Fetch as associative array
+
+        if ($data === false) {
+            echo "The user does not exist";
+        } else {
+            if (password_verify($password, $data['password'])) {
+                $_SESSION['id'] = $data['id'];
+                $_SESSION['username'] = $data['username'];
+                $_SESSION['email'] = $data['email'];
+                $_SESSION['is_admin'] = $data['is_admin'];
+
+                header('Location: dashboard.php');
+                exit();
+            } else {
+                echo "The password is not valid";
+            }
+        }
+    }
 }
-
-}
-
-
-
 ?>
